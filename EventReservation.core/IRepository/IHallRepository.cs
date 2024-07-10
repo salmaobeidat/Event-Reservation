be0d7ad10b7566abe6e7f0c38afbc2810cfa5355
salmaobeidat <obeidatsalma5@gmail.com>
2024-07-11 01:56:44 +0300

    Hall Repository create method

---
+++ EventReservation.core/IRepository/IHallRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventReservation.core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventReservation.core.IRepository
+{
+    public interface IHallRepository
+    {
+        Task<List<Hall>> GetAllHalls();
+        Task<Hall> GetHallById(int id);
+        Task CreateHall(Hall hall);
+        Task UpdateHall(Hall hall);
+        Task DeleteHall(int id);
+    }
+}
